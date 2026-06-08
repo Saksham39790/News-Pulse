cdd0a53b18bbfb62148b0d24a63a766deabc9f41
Saksham39790 <your-github-email@example.com>
2026-06-09 02:09:21 +0530

    Added Render environment variable support

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.static("public"));
 
-const API_KEY = "decf1134dac7472da25a139dda23a861";
+const API_KEY = process.env.GNEWS_API_KEY;
 
 app.get("/news", async (req, res) => {
     try {
@@ -14,7 +14,7 @@ app.get("/news", async (req, res) => {
         const page = req.query.page || 1;
 
         const response = await fetch(
-            `https://newsapi.org/v2/everything?q=${search}&sortBy=publishedAt&pageSize=20&page=${page}&apiKey=${API_KEY}`
+        `https://gnews.io/api/v4/search?q=${search}&lang=en&max=20&apikey=2e772ce90279db0751ddca2877396962`
         );
 
         const data = await response.json();
@@ -22,7 +22,19 @@ app.get("/news", async (req, res) => {
         console.log("Search:", search);
         console.log("Results:", data.totalResults);
 
-        res.json(data);
+        const formattedArticles = data.articles.map(article => ({
+            title: article.title,
+            description: article.description,
+            url: article.url,
+            urlToImage: article.image,
+            source: {
+                name: article.source.name
+            }
+        }));
+
+res.json({
+    articles: formattedArticles
+});
 
     } catch (error) {
         console.error(error);
