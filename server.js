a2829956cde55d94bc9d54cca33f5cf24013733b
Saksham39790 <your-github-email@example.com>
2026-06-09 20:18:43 +0530

    Remove debug logs

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ app.get("/news", async (req, res) => {
         console.log("Search:", search);
         console.log("Results:", data.totalResults);
         console.log("Articles returned:", data.articles.length);
-        console.log("First article:", data.articles[0]);
 
         const formattedArticles = data.articles.map(article => ({
             title: article.title,
